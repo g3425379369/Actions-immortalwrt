40affc5a94b8f51bc4008eb8aeea5953e96389bc
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-03-14 11:43:35 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -11,8 +11,8 @@
 #
 
 # hlk7628n dts
-# mkdir -p target/linux/ramips/dts/
-# cp -f "$GITHUB_WORKSPACE/dts/mt7628an_hilink_hlk-7628n.dts" target/linux/ramips/dts/mt7628an_hilink_hlk-7628n.dts
+mkdir -p target/linux/ramips/dts/
+cp -f "$GITHUB_WORKSPACE/dts/mt7628an_hilink_hlk-7628n.dts" target/linux/ramips/dts/mt7628an_hilink_hlk-7628n.dts
 
 
 # turboacc
