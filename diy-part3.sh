8add643842b141f9003c60d8842c300636abfe3d
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-03-17 14:40:53 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -19,7 +19,5 @@ cp -f "$GITHUB_WORKSPACE/dts/mt7628an_hilink_hlk-7628n.dts" target/linux/ramips/
 # curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
 
-# packages
-sed -i 's|https://github.com/coolsnowwolf/packages|https://github.com/xuxin1955/packages|g' feeds.conf.default
 
 
