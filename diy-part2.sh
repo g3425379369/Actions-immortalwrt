14c7088c03f5cadac7550335d368288623120a22
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-04-29 18:22:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,17 +17,6 @@ git clone https://github.com/lkiuyu/luci-app-temp-status package/luci-app-temp-s
 # DbusSmsForwardCPlus
 git clone https://github.com/lkiuyu/DbusSmsForwardCPlus package/DbusSmsForwardCPlus
 
-# 改 libmbim
-sed -i 's/DEPENDS:=+glib2/DEPENDS:=+glib2 +glib2-gio +glib2-gobject +glib2-core/' package/feeds/packages/libmbim/Makefile
-
-# 改 libqrtr-glib
-sed -i 's/DEPENDS:=+glib2/DEPENDS:=+glib2 +glib2-gio +glib2-gobject +glib2-core/' package/feeds/packages/libqrtr-glib/Makefile
-
-# 验证
-echo "=== libmbim ==="
-grep "DEPENDS" package/feeds/packages/libmbim/Makefile
-echo "=== libqrtr-glib ==="
-grep "DEPENDS" package/feeds/packages/libqrtr-glib/Makefile
 
 
 
