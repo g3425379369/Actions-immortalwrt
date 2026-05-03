ce5f1a98e48d392a9e579bb7c34a16772febca4c
xuxin1955 <x2874836915@qq.com>
2026-05-03 11:54:41 +0800

    update

--- diy-part3.sh
+++ diy-part3.sh
@@ -8,9 +8,13 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 mkdir -p target/linux/ramips/dts/
 cp -f "$GITHUB_WORKSPACE/scripts/dts/mt7628an_hilink_hlk-7628n.dts" "target/linux/ramips/dts/mt7628an_hilink_hlk-7628n.dts"
 
-# hlk7628n (LEDs配置)
+# hlk7628n (LEDs)
 # mkdir -p "target/linux/ramips/mt76x8/base-files/etc/board.d/"
 # cp -f "$GITHUB_WORKSPACE/scripts/image/01_leds" "target/linux/ramips/mt76x8/base-files/etc/board.d/01_leds"
+
+# hlk7628n (LEDs)
+mkdir -p package/kernel/mt76/
+cp -f "$GITHUB_WORKSPACE/scripts/image/mt76-Makefile" "package/kernel/mt76/Makefile"
 
 # turboacc
 # curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
