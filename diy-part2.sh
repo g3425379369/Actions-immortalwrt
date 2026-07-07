1770437bb0108a48aef53f37bbb0d17706c34069
g3425379369 <86862449+g3425379369@users.noreply.github.com>
2026-07-07 13:21:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,13 +2,19 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-sed -i 's/luci-theme-material/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-material/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # DbusSmsForwardCPlus
 git clone https://github.com/lkiuyu/DbusSmsForwardCPlus package/DbusSmsForwardCPlus
+
+git clone https://github.com/gSpotx2f/luci-app-cpu-perf.git package/luci-app-cpu-perf
+git clone https://github.com/gSpotx2f/luci-app-cpu-status.git package/luci-app-cpu-status
+git clone https://github.com/gSpotx2f/luci-app-temp-status.git package/luci-app-temp-status
+
+git clone https://github.com/g3425379369/UA3F.git package/UA3F
 
 # 北大源
 cp -r "$GITHUB_WORKSPACE/scripts/files-8916" "$GITHUB_WORKSPACE/openwrt/files"
