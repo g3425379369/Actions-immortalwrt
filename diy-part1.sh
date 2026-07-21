6cbbb5394a236540007c85d92f875550621f54b1
g3425379369 <86862449+g3425379369@users.noreply.github.com>
2026-07-21 16:59:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,3 +2,6 @@
 
 # turboacc
 # curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+
+rm -rf "$GITHUB_WORKSPACE/openwrt/package/kernel/r8152"
+cp -rf "$GITHUB_WORKSPACE/scripts/imm_8916/"* "$GITHUB_WORKSPACE/openwrt/"
