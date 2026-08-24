dd4b3d5a08d477f8fda7ec3f63dab7b65a69a93f
g3425379369 <86862449+g3425379369@users.noreply.github.com>
2026-08-24 12:49:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,5 +3,6 @@
 # turboacc
 # curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
+rm -rf "$GITHUB_WORKSPACE/openwrt/package/system/apk"
 rm -rf "$GITHUB_WORKSPACE/openwrt/package/kernel/r8152"
 cp -rf "$GITHUB_WORKSPACE/scripts/imm_8916/"* "$GITHUB_WORKSPACE/openwrt/"
